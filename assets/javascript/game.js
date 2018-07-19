4cdebcfa772323c2fd11d8959bdb83172fe9728c
Joseph Venuto <jbvenuto@gmail.com>
2018-07-18 20:49:43 -0400

    wrote comments to plan out js logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,26 @@
 //Array with words to fit the scifi theme
-var words = ["stars", "space", "spaceship", "alien", "interstellar", "galaxy", "planet","nebula"]
-var arrLength = words.length;
+var wordList = ["stars", "space", "alien", "galaxy", "planet","nebula"]
+var arrLength = wordList.length;
+
+//Select a word from the available words
+var i = Math.floor(Math.random() * arrLength);
+var word = wordList[i];
+var j = word.length;
+
+//Make an array that is the same length as the word and full of *
+
+//Get the user's letter guess
+document.onkeyup = function(event) {}
+
+
+//Put the letter into the word if it was a correct guess
+
+
+//Put the letter into a list of wrongly guessed letters
+
+
+//Logs variables to the console
+console.log(arrLength);
+console.log(wordList);
+console.log(word);
+console.log(i);
